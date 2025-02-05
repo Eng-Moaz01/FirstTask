8a2eb437e174bf6c1835418300d3bb0940ec82e8
Eng-Moaz01 <mmezohrhera@gmail.com>
2025-02-05 15:24:16 +0200

    while

--- First task;.py
+++ First task;.py
@@ -1,12 +1,20 @@
-degree = int(input("enter your degree :-> "))
+isRun=True
 
-if 100 >= degree >= 85:
-    print("Excellent")
-elif 85 > degree >= 75:
-    print("VEry Good")
-elif 75 > degree >= 65:
-    print("good")
-elif 65 > degree >= 50:
-    print("acceptable")
-else:
-    print("fail")
+while isRun:
+    degree = int(input("enter your degree for exit enter -1 :-> "))
+
+    if degree == -1:
+        isRun=False
+        break
+
+
+    if 100 >= degree >= 85:
+        print("Excellent")
+    elif 85 > degree >= 75:
+        print("VEry Good")
+    elif 75 > degree >= 65:
+        print("good")
+    elif 65 > degree >= 50:
+        print("acceptable")
+    else:
+        print("fail")
