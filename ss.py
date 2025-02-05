4a40f7b3e998665be020a1c52f9992fddea216ea
Eng-Moaz01 <mmezohrhera@gmail.com>
2025-02-05 14:57:13 +0200

    Remov def g_degree because There is no benefit from it

--- ss.py
+++ ss.py
@@ -1,14 +1,5 @@
 degree = int(input("enter your degree :-> "))
 
-
-
-
-def g_degree (values):
-    degree=""
-    for i in range(0,100) :
-        degree =degree + str(degree.join(random.choices(values,k=1)))
-    return degree
-
 if(degree<=100 and degree >=85):
     print("Excellent")
 elif(degree<85 and degree>= 75):
